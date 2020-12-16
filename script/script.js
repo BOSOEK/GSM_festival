fb44c015a913cbe933fa63b915a7dcd0a7235394
BOSOEK <thydcj@naver.com>
2020-12-16 15:27:20 +0900

    로그인 창 완성

--- script/script.js
+++ script/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //list_icon 버튼이 클릭되면 right_hiddin창 보이게 하기
 var list_icon_check = 0;
 $('#list_icon').click(function () {
@@ -25,19 +24,4 @@ $('#h2_hid_img').click(function() {
         h2_hiddin_check = 0;
         $('#up_down_img').attr('src', './Image/down_arrow.png');
     }
-=======
-//list_icon 버튼이 클릭되면 right_hiddin창 보이게 하기
-var list_icon_check = 0;
-$('#list_icon').click(function () {
-    if(list_icon_check == 0) {
-        $('#right_hiddin').animate({"left" : "-=39%"});
-        $('#list_icon_img').attr('src', 'Image/list_icon_X.png');
-        list_icon_check = 1;
-    } else {
-        $('#right_hiddin').animate({"left" : "+=39%"});
-        $('#list_icon_img').attr('src', 'Image/list_icon.png');
-        list_icon_check = 0;
-    }
-
->>>>>>> fb25a693de3f2c79fc497aaf0b6e87313549be3c
 });
