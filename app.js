97d174a8c8423f1312a8d7ce79c92e4fd8eda9f1
BOSOEK <thydcj@naver.com>
2020-12-30 01:29:18 +0900

    html파일들 모두 파일 위치 연결 성공

--- app.js
+++ app.js
@@ -6,14 +6,17 @@ const router = express.Router();
 
 const app = express();
 
+app.use(express.static(__dirname + '/Image'));
+app.use(express.static(__dirname + '/html'));
+app.use(express.static(__dirname + '/style'));
+app.use(express.static(__dirname + '/script'));
+
 const db = mysql.createConnection( {
     host: '192.168.154.1',
     user: 'client',
     password: '33945',
     database: 'GSM'
 });
-const publicDirectory = path.join(__dirname, './style');
-app.use(express.static(publicDirectory));
 
 //URL 인코딩 본문 구문 분석 (HTML 양식에서 전송 됨)
 app.use(express.urlencoded({extended:false}));
@@ -30,7 +33,11 @@ db.connect(function(err) {
     }
 })
 
-app.use('/', require('./script/pages.js'));
+app.get('/', function(req, res) {
+    res.status(200).sendFile(__dirname + "/html/index.html");
+});
+
+//app.use('/', require('./script/pages.js'));
 
 app.listen(9996, function() {
     console.log("Server Started!");
