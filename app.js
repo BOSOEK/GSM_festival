280f5a2024c7ab5eee125997648251ef5fb8093f
BOSOEK <thydcj@naver.com>
2020-12-29 23:57:22 +0900

    html파일을 hbs로 변환

---
+++ app.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const mysql = require('mysql');
+const express = require('express');
+
+const app = express();
+
+const server = http.createServer();
+
+const db = mysql.createConnection( {
+    host: '192.168.154.1',
+    user: 'client',
+    password: '33945',
+    database: 'GSM'
+});
+
+//URL 인코딩 본문 구문 분석 (HTML 양식에서 전송 됨)
+app.use(express.urlencoded({extended:false}));
+//JSON 본문 구문 분석 (API 클라이언트에서 보낸대로)
+app.use(express.json());
+
+app.set('view engine', 'hbs');
+
+db.connect(function(err) {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log("MYSQL Connected...");
+    }
+})
+
+app.listen(6997, function() {
+    console.log("Server Started!");
+});
