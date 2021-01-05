3c855863828746e9939ec280a9e6850b409124fd
BOSOEK <thydcj@naver.com>
2021-01-06 02:37:01 +0900

    commit

--- script/script.js
+++ script/script.js
@@ -1,12 +1,14 @@
+const { text } = require("body-parser");
+
 //list_icon 버튼이 클릭되면 right_hiddin창 보이게 하기
 var list_icon_check = 0;
 $('#list_icon').click(function () {
     if(list_icon_check == 0) {
-        $('#right_hiddin').animate({"left" : "-=39%"});
+        $('#right_hiddin').animate({"left" : "-=100%"}, 800);
         $('#list_icon_img').attr('src', './list_icon_X.png');
         list_icon_check = 1;
     } else {
-        $('#right_hiddin').animate({"left" : "+=39%"});
+        $('#right_hiddin').animate({"left" : "+=100%"}, 800);
         $('#list_icon_img').attr('src', './list_icon.png');
         list_icon_check = 0;
     }
@@ -25,3 +27,5 @@ $('#h2_hid_img').click(function() {
         $('#up_down_img').attr('src', './down_arrow.png');
     }
 });
+
+$('#text1').hide();
